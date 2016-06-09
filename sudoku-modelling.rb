280cd6e247c74a87f071f4e3d1a89afd05c1a6b7
Guest User <aleah.luk@gmail.com>
2016-06-09 18:15:42 +0800

    so close

--- sudoku-modelling.rb
+++ sudoku-modelling.rb
@@ -52,13 +52,14 @@ class Sudoku
         [0, 1, 2, 0, 1, 2, 0, 1, 2],
         [3, 4, 5, 3, 4, 5, 3, 4, 5],
         [6, 7, 8, 6, 7, 8, 6, 7, 8]]
-        @empty_array = Array.new(9){Array.new(9)}
-        p @board_horizontal
-        puts ""
-        p @board_vertical
-        puts ""
-        p @board_squares
-        puts ""
+        @possibilities = Array.new(9){Array.new(9)}
+        for i in (0..8)
+            for j in (0..8)
+                if @board_horizontal[i][j] == 0
+                    @possibilities[i][j] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+                end
+            end
+        end
     end
     
     def solve!
@@ -66,42 +67,50 @@ class Sudoku
             for x in (0..8)
                 for y in (0..8)
                     if @board_horizontal[x][y] == 0
-                        possibilities = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-                        @board_horizontal.each {|value| possibilities.delete(value) if value != 0}
-                        if possibilities.length ==
-                            
-                            # until @board_horizontal.each.include?(0) == false
-                            # 	@board_horizontal.each do |line|
-                            # 		line.each do |number|
-                            # 			if number == 0
-                            # 			possibilities = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-                            # 			line.each {|x| possibilities.delete(x) if x != 0}
-                            # 				if possibilities.length == 1
-                            # 					number = possibilities[0]
-                            # 				end
-                            # 			end
-                            # 		end
-                            # 	end
-                            # end
+                        @board_horizontal.each {|value| @possibilities[x][y].delete(value) if value != 0}
+                        if @possibilities[x][y].length == 1
+                            @board_horizontal[x][y] = @possibilities[x][y][0].dup
+                            @board_vertical[y][x] = @possibilities[x][y][0].dup
+                            @board_squares[@squares_x[x]][@squares_y[y]] = @possibilities[x][y][0].dup
                         end
-                        p @board_horizontal
                     end
-                    
-                    # Returns a string representing the current state of the board
-                    # Don't spend too much time on this method; flag someone from staff
-                    # if you are.
-                    def board
-                    
+                    if @board_vertical[x][y] == 0
+                        @board_vertical.each {|value| @possibilities[y][x].delete(value) if value != 0}
+                        if @possibilities[y][x].length == 1
+                            @board_horizontal[y][x] = @possibilities[y][x][0].dup
+                            @board_vertical[x][y] = @possibilities[y][x][0].dup
+                            @board_squares[@squares_x[y]][@squares_y[x]] = @possibilities[y][x][0].dup
+                        end
+                    end
+                    if @board_squares[x][y] == 0
+                        @board_squares.each {|value| @possibilities[@squares_x[x]][@squares_y[y]].delete(value) if value != 0}
+                        if @possibilities[@squares_x[x]][@squares_y[y]].length == 1
+                            @board_horizontal[@squares_x[x]][@squares_y[y]] = @possibilities[@squares_x[x]][@squares_y[y]][0].dup
+                            @board_vertical[@squares_x[y]][@squares_y[x]] = @possibilities[@squares_x[x]][@squares_y[y]][0].dup
+                            @board_squares[x][y] = @possibilities[@squares_x[x]][@squares_y[y]][0].dup
+                        end
+                    end
                 end
             end
-            
-            # The file has newlines at the end of each line, so we call
-            # String#chomp to remove them.
-            #board_string = File.readlines('sample.unsolved.txt').first.chomp
-            
-            game = Sudoku.new(619030040270061008000047621486302079000014580031009060005720806320106057160400030)
-            
-            # Remember: this will just fill out what it can and not "guess"
-            game.solve!
-            
-            puts game.board
+        end
+        p @board_horizontal
+    end
+    
+    # Returns a string representing the current state of the board
+    # Don't spend too much time on this method; flag someone from staff
+    # if you are.
+    def board
+        
+    end
+end
+
+# The file has newlines at the end of each line, so we call
+# String#chomp to remove them.
+#board_string = File.readlines('sample.unsolved.txt').first.chomp
+
+game = Sudoku.new(619030040270061008000047621486302079000014580031009060005720806320106057160400030)
+
+# Remember: this will just fill out what it can and not "guess"
+game.solve!
+
+puts game.board
