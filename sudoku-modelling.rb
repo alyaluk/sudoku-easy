6709f8c5e5326ae76a15d445ad06d99b1dff3874
Guest User <aleah.luk@gmail.com>
2016-06-10 11:23:32 +0800

    Thanks to chanmw96, I used a lot of your methodology in my final solve code. Cut it down by a few lines, but it would be even shorter with your version of initialize

--- sudoku-modelling.rb
+++ sudoku-modelling.rb
@@ -1,14 +1,7 @@
 class Sudoku
     def initialize(board_string)
-        @board_horizontal = []
-        horizontal_arr = board_string.to_s.split("").map { |digit| digit.to_i }
-        9.times do
-            temp_board = []
-            selected = horizontal_arr.take(9)
-            selected.each {|x| temp_board << x}
-            horizontal_arr = horizontal_arr.drop(9)
-            @board_horizontal << temp_board
-        end
+        @horizontal_arr = board_string.to_s.split("").map { |digit| digit.to_i }
+        @board_horizontal = @horizontal_arr.each_slice(9).to_a
         @board_squares = []
         square_arr = @board_horizontal.dup
         3.times do
@@ -23,84 +16,53 @@ class Sudoku
             end
             square_arr = square_arr.drop(3)
         end
-        vertical_arr = @board_horizontal.dup
-        @board_vertical = []
-        9.times do
-            temp_board = []
-            for i in (0..8)
-                selected = vertical_arr[i].take(1)
-                selected.each {|x| temp_board << x}
-                vertical_arr[i] = vertical_arr[i].drop(1)
-            end
-            @board_vertical << temp_board
-        end
-        @squares_x = [[0, 0, 0, 1, 1, 1, 2, 2, 2],
-        [0, 0, 0, 1, 1, 1, 2, 2, 2],
-        [0, 0, 0, 1, 1, 1, 2, 2, 2],
-        [3, 3, 3, 4, 4, 4, 5, 5, 5],
-        [3, 3, 3, 4, 4, 4, 5, 5, 5],
-        [3, 3, 3, 4, 4, 4, 5, 5, 5],
-        [6, 6, 6, 7, 7, 7, 8, 8, 8],
-        [6, 6, 6, 7, 7, 7, 8, 8, 8],
-        [6, 6, 6, 7, 7, 7, 8, 8, 8]]
-        @squares_y = [[0, 1, 2, 0, 1, 2, 0, 1, 2],
-        [3, 4, 5, 3, 4, 5, 3, 4, 5],
-        [6, 7, 8, 6, 7, 8, 6, 7, 8],
-        [0, 1, 2, 0, 1, 2, 0, 1, 2],
-        [3, 4, 5, 3, 4, 5, 3, 4, 5],
-        [6, 7, 8, 6, 7, 8, 6, 7, 8],
-        [0, 1, 2, 0, 1, 2, 0, 1, 2],
-        [3, 4, 5, 3, 4, 5, 3, 4, 5],
-        [6, 7, 8, 6, 7, 8, 6, 7, 8]]
-        @possibilities = Array.new(9){Array.new(9)}
-        for i in (0..8)
-            for j in (0..8)
-                if @board_horizontal[i][j] == 0
-                    @possibilities[i][j] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-                end
-            end
+        @counter = @horizontal_arr.count(0)
+    end
+    
+    def locate(value)
+        x = value / 9
+        y = value % 9
+        if x < 3
+            return @board_squares[0] if y < 3
+            return @board_squares[1] if y < 6
+            return @board_squares[2] if y < 9
+            elsif x < 6
+            return @board_squares[3] if y < 3
+            return @board_squares[4] if y < 6
+            return @board_squares[5] if y < 9
+            elsif x < 9
+            return @board_squares[6] if y < 3
+            return @board_squares[7] if y < 6
+            return @board_squares[8] if y < 9
         end
     end
     
     def solve!
-        until @board_horizontal.each.include?(0) == false && @board_vertical.each.include?(0) == false && @board_squares.each.include?(0) == false
-            for x in (0..8)
-                for y in (0..8)
-                    if @board_horizontal[x][y] == 0
-                        @board_horizontal.each {|value| @possibilities[x][y].delete(value) if value != 0}
-                        if @possibilities[x][y].length == 1
-                            @board_horizontal[x][y] = @possibilities[x][y][0].dup
-                            @board_vertical[y][x] = @possibilities[x][y][0].dup
-                            @board_squares[@squares_x[x]][@squares_y[y]] = @possibilities[x][y][0].dup
-                        end
-                    end
-                    if @board_vertical[x][y] == 0
-                        @board_vertical.each {|value| @possibilities[y][x].delete(value) if value != 0}
-                        if @possibilities[y][x].length == 1
-                            @board_horizontal[y][x] = @possibilities[y][x][0].dup
-                            @board_vertical[x][y] = @possibilities[y][x][0].dup
-                            @board_squares[@squares_x[y]][@squares_y[x]] = @possibilities[y][x][0].dup
-                        end
-                    end
-                    if @board_squares[x][y] == 0
-                        @board_squares.each {|value| @possibilities[@squares_x[x]][@squares_y[y]].delete(value) if value != 0}
-                        if @possibilities[@squares_x[x]][@squares_y[y]].length == 1
-                            @board_horizontal[@squares_x[x]][@squares_y[y]] = @possibilities[@squares_x[x]][@squares_y[y]][0].dup
-                            @board_vertical[@squares_x[y]][@squares_y[x]] = @possibilities[@squares_x[x]][@squares_y[y]][0].dup
-                            @board_squares[x][y] = @possibilities[@squares_x[x]][@squares_y[y]][0].dup
-                        end
-                    end
+        until @counter == 0
+            @counter = @horizontal_arr.count(0)
+            zeros = []
+            @horizontal_arr.each_with_index {|value, place| zeros << place if value == 0}
+            zeros.each do |x|
+                possible = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+                possible -= @board_horizontal[x/9]
+                possible -= @board_horizontal.transpose[x%9]
+                position = locate(x)
+                possible -= position
+                if possible.length == 1
+                    @board_horizontal[x/9][x%9] = possible[0]
+                    @counter -= 1
+                    zeros -= [x]
                 end
             end
+            @horizontal_arr = @board_horizontal.flatten
         end
-        p @board_horizontal
     end
     
     # Returns a string representing the current state of the board
     # Don't spend too much time on this method; flag someone from staff
     # if you are.
     def board
-        
+        @board_horizontal.flatten.join("")
     end
 end
 
@@ -108,7 +70,7 @@ end
 # String#chomp to remove them.
 #board_string = File.readlines('sample.unsolved.txt').first.chomp
 
-game = Sudoku.new(619030040270061008000047621486302079000014580031009060005720806320106057160400030)
+game = Sudoku.new(105802000090076405200400819019007306762083090000061050007600030430020501600308900)
 
 # Remember: this will just fill out what it can and not "guess"
 game.solve!
